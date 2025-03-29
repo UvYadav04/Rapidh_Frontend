eeef23f4480823f6164da3c9971298c93dda78e6
Dinesh Yadav <dineshnirban01@gmail.com>
2025-03-29 11:41:11 +0530

    corrected loader position

--- src/Components/Authentication/LoginLoader.tsx
+++ src/Components/Authentication/LoginLoader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Loader.css'
 function LoginLoader() {
     return (
-        <div className="w-full h-full loader z-[100]">
+        <div className="w-[100vw] h-[100vh] fixed  top-0 left-0  loader z-[100]">
             <div className="justify-content-center jimu-primary-loading"></div>
         </div>
     )
